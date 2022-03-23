c6924a65cdd9ef92f3b49596b8e0c39ee7c3cbdd
THANATHORN <99862291+THANAYHORN@users.noreply.github.com>
2022-03-23 18:01:15 +0000

    Initial commit

---
+++ pages/api/hello.js
@@ -0,0 +1,19 @@
+export default(req, res) => {
+  res.status(200).json([
+  {
+    name: 'Product_1',
+    image: 'https://cdn.store-assets.com/s/875875/i/39054424.png',
+    company: 'THANA Business.'
+  },
+  {
+    name: 'Product_1',
+    image: 'https://cdn.store-assets.com/s/875875/i/39054424.png',
+    company: 'THANA Business.'
+  },
+  {
+    name: 'Product_1',
+    image: 'https://cdn.store-assets.com/s/875875/i/39054424.png',
+    company: 'THANA Business.'
+  }
+])
+}
---
+++ pages/api/hello1.js
@@ -0,0 +1,20 @@
+export default(req, res) => {
+  res.status(200).json([
+  {
+    name: 'Product_1',
+    image: 'https://cdn.store-assets.com/s/875875/i/39054424.png',
+    company: 'THANA Business.'
+  },
+  {
+    name: 'Product_1',
+    image: 'https://cdn.store-assets.com/s/875875/i/39054424.png',
+    company: 'THANA Business.'
+  },
+  {
+    name: 'Product_1',
+    image: 'https://cdn.store-assets.com/s/875875/i/39054424.png',
+    company: 'THANA Business.'
+  }
+])
+}
+console.log("OK")
