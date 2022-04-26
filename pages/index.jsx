cdc020beaa968a82c87b0171e37b92e76955c30d
THANATHORN <99862291+THANABusiness@users.noreply.github.com>
2022-04-26 08:34:00 +0700

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -65,3 +65,15 @@ export default function Home() {
     </div>
   )
 }
+
+<!-- Hotjar Tracking Code for https://thanabusiness-next.vercel.app -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2940917,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
